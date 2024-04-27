260219e0da56f116bc006ffe6424d20fc6e12702
Lorenzo Peve <lpeve01@gmail.com>
2024-04-27 18:46:32 -0500

    Update models.py

--- fitness_tracker/app/models.py
+++ fitness_tracker/app/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from django.db import models
+
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=20, primary_key=True)
+    first_name = models.CharField(max_length=30, null=True)
+    last_name = models.CharField(max_length=30, null=True)
+    dob = models.DateField()
+    email = models.EmailField(max_length=50)
+    city = models.CharField(max_length=30, null=True)
+    state = models.CharField(max_length=30, null=True)
+    country = models.CharField(max_length=30, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
