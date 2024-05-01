00d1b5f61cfbecb88999b6ff33cc76242f43eee0
Lorenzo Peve <lpeve01@gmail.com>
2024-04-30 21:41:12 -0500

    Update urls.py

--- fitness_tracker/app/urls.py
+++ fitness_tracker/app/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("logexercise", views.add_exercise, name="add_exercise"),
     path("logexercise/sucess", views.add_exercise_sucess, name="add_exercise_sucess"),
     path("logexercise/error", views.add_exercise_failure, name="add_exercise_failure"),
-    path("exercises", views.test, name="get_user_exercises"),
+    path("exercises", views.get_user_exercises, name="get_user_exercises"),
 ]
